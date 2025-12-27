8417164843bb68532e9b571025a5946bb29b5ea2
HAJPRO <umidshomurodov265@gmail.com>
2025-12-28 02:04:31 +0500

    Birinchi commit

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+// app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 // ------------------ ROUTES ------------------
 // Bots
